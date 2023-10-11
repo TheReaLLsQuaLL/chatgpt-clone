28468c20b912e7f7320a0978f124824ff6431a37
TheReaLLsQuaLL <toprakyaramaz@hotmail.com>
2023-10-11 20:17:19 +0300

    feat: mostly the website look done now looks more like chatgpt

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ function App() {
     <div className="app">
       <section className="side-bar">
         <button>+ New Chat</button>
-        <ul className="history "></ul>
+        <ul className="history ">
+          <li>Example</li>
+        </ul>
         <nav>
           <p>Made by Toprak YILDIZ</p>
         </nav>
       </section>
-      <section className="main"></section>
+      <section className="main">
+        <h1>Clone-GPT</h1>
+        <ul className="feed"></ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">
+            Clone-GPT version over 9000. We simply here guarantee here all this
+            work is a clone of Chat-GPT and the coder is simply not skillfull
+            enough to make his own thing.Yes i am talking to you Toprak.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
