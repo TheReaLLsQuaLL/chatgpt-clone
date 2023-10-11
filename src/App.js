1fcd270858e350fec05ba0d2f546444e65c64af2
TheReaLLsQuaLL <toprakyaramaz@hotmail.com>
2023-10-11 19:26:56 +0300

    feat: Added some sidebar features and also deleted some info

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history "></ul>
+        <nav>
+          <p>Made by Toprak YILDIZ</p>
+        </nav>
+      </section>
+      <section className="main"></section>
+    </div>
+  );
 }
 
 export default App;
